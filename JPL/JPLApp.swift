88aa85964683ac7ea0e6b8f732800ac0a299dbcd
ischarlotte <102317303+ischarlotte@users.noreply.github.com>
2024-12-09 20:22:34 +0900

    Initial Commit

---
+++ JPL/JPLApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPLApp.swift
+//  JPL
+//
+//  Created by 大帅哥 on R 6/12/09.
+//
+
+import SwiftUI
+
+@main
+struct JPLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
